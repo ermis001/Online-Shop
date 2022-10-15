bb7047420d197fcfe9d0dc08c1b97593ed3890a7
ermis001 <ermis.dauti1@gmail.com>
2022-10-15 14:13:59 +0200

    alalallaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutProductComponent } from 'src/User-Interference/about-product/about-product.component';
-import { AboutUsComponent } from 'src/User-Interference/about-us/about-us.component';
+import { AboutProductComponent } from './User-Interference/about-product/about-product.component';
+import { AboutUsComponent } from './User-Interference/about-us/about-us.component';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingCardModule } from './shopping-card/shopping-card.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { SdaHttpClientModule } from './data-layer/sda-be-mock.module';
-import { ProductListComponent } from 'src/Admin-Interference/product-list/product-list.component';
-import { ProductDetailsComponent } from 'src/Admin-Interference/product-details/product-details.component';
-import { CategoryListComponent } from 'src/Admin-Interference/category-list/category-list.component';
-
+import { ProductListComponent } from './Admin-Interference/product-list/product-list.component';
+import { ProductDetailsComponent } from './Admin-Interference/product-details/product-details.component';
+import { CategoryListComponent } from './Admin-Interference/category-list/category-list.component';
 @NgModule({
   declarations: [
     AppComponent,
