5301af4a87033ab9fb213e713384258c92082bf7
ermis001 <ermis.dauti1@gmail.com>
2022-10-15 12:21:38 +0200

    adminUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingCardModule } from './shopping-card/shopping-card.module';
 import { ShoppingModule } from './shopping/shopping.module';
 import { SdaHttpClientModule } from './data-layer/sda-be-mock.module';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutProductComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
