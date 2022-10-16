94d708e21b4efa8ea1e3d66dd3d02caf4b492883
DAZE\User <igli.ylli1594@gmail.com>
2022-10-16 09:19:03 +0200

    product list finished

--- src/app/Admin-Interference/product-list/product-list.component.ts
+++ src/app/Admin-Interference/product-list/product-list.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-
+  data:any[]=[
+    {id:1,name:'Pants',category:'Clothing',price:12.99 },
+    {id:2,name:'T-shirt',category:'Men Clothing',price:16.99},
+    {id:3,name:'Shoes',category:'Mens Sports Wears',price:13.99},
+    {id:1,name:'Pants',category:'Clothing',price:12.99 },
+    {id:2,name:'T-shirt',category:'Men Clothing',price:16.99},
+    {id:3,name:'Shoes',category:'Mens Sports Wears',price:13.99},
+  ]
+   
   constructor() { }
 
   ngOnInit(): void {
