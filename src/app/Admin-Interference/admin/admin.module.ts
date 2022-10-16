4d9d7cb32c6fa4505f38e0bc4b7ebf136b4e70e7
DAZE\User <igli.ylli1594@gmail.com>
2022-10-16 12:23:20 +0200

    comit

--- src/app/Admin-Interference/admin/admin.module.ts
+++ src/app/Admin-Interference/admin/admin.module.ts
@@ -25,8 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
         path:'app-product-list',component:ProductListComponent
       },
       {
-        path: 'product-details',
-        component: ProductDetailsComponent,
+        path: 'product-details',component: ProductDetailsComponent,
+        
       },
     ])
   ]
