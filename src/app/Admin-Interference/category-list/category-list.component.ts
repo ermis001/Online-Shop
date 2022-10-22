ecd2a0845652bcffcac5180d6c8c51a3142683e5
DAZE\User <igli.ylli1594@gmail.com>
2022-10-22 15:04:32 +0200

    fixes

--- src/app/Admin-Interference/category-list/category-list.component.ts
+++ src/app/Admin-Interference/category-list/category-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-category-list',
@@ -9,9 +10,17 @@ export class CategoryListComponent implements OnInit {
   categoryButtonToInput:boolean = true;
   editButtonToInput:boolean = true;
 
-  constructor() { }
+  categoryFormGroup!:FormGroup;
+
+  constructor(private builder:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.categoryFormGroup= new FormGroup(
+      {
+        name: new FormControl([null, Validators.required])
+      }
+    ) 
   }
 
   buttonTriger (triger: boolean){
