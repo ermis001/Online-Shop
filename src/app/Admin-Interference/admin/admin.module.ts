aaa2df3169f9a499c57d2ad9fad1ccc0a1c767ee
DAZE\User <igli.ylli1594@gmail.com>
2022-10-24 01:23:58 +0200

    admin finished

--- src/app/Admin-Interference/admin/admin.module.ts
+++ src/app/Admin-Interference/admin/admin.module.ts
@@ -8,30 +8,25 @@ import { ProductDetailsComponent } from '../product-details/product-details.comp
 import { CategoryListComponent } from '../category-list/category-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
 @NgModule({
   declarations: [
     AdminLayoutComponent,
     ProductListComponent,
     ProductDetailsComponent,
     CategoryListComponent,
-
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule.forChild([
       {
-        path:'app-product-list',component:ProductListComponent
+        path: 'app-product-list',
+        component: ProductListComponent,
       },
-      {
-        path: 'product-details',component: ProductDetailsComponent,
-        
-      },
-      {path: 'product-details/:id',component: ProductDetailsComponent}
-    ])
-  ]
+     
+      { path: 'category-details/:id', component: CategoryListComponent },
+      { path: 'product-details/:id', component: ProductDetailsComponent },
+    ]),
+  ],
 })
-        
-        
-export class AdminModule { }
+export class AdminModule {}
--- src/app/Admin-Interference/product-list/product-list.component.ts
+++ src/app/Admin-Interference/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
 
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,12 +11,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
   data: any;
   productTable: any;
- 
+  categoriesTable:any;
 
-  constructor( private productService: ProductService) {}
+  constructor(private categoryService:CategoryService, private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProduct();
+    this.getCategories();
   }
 
   getProduct() {
@@ -24,10 +26,22 @@ export class ProductListComponent implements OnInit {
     });
   }
 
+  getCategories(){
+    this.categoryService.getCategory().subscribe((result) => {
+      this.categoriesTable = result
+    })
+  }
+
   deleteProduct(id: number) {
     this.productService.deleteProduct(id).subscribe(() => {
       this.getProduct();
     });
   }
 
+  deleteCategory(id: number){
+    this.categoryService.deleteCategory(id).subscribe(() => {
+      this.getCategories()
+    })
+  }
+  
 }
