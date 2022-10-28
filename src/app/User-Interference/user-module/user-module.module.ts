56b121af0fc936cbf298e8f73131d662f1a8bc31
ermis001 <ermis.dauti1@gmail.com>
2022-10-28 16:13:58 +0200

    small changes

--- src/app/User-Interference/user-module/user-module.module.ts
+++ src/app/User-Interference/user-module/user-module.module.ts
@@ -7,7 +7,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { ShoppingCardModule } from 'src/app/shopping-card/shopping-card.module';
 import { AboutProductComponent } from '../about-product/about-product.component';
 import { AboutUsComponent } from '../about-us/about-us.component';
-
+import { ShopingPageComponent } from 'src/app/shopping/shoping-page/shoping-page.component';
 
 
 @NgModule({
@@ -19,7 +19,10 @@ import { AboutUsComponent } from '../about-us/about-us.component';
     CommonModule,SharedModule,ShoppingCardModule,
     RouterModule.forChild([
       {
-        path:'',component:HomeComponent
+        path:'',component:HomeComponent,
+      },
+      {
+        path:'product-list', component:ShopingPageComponent,
       }
     ])
   ]
