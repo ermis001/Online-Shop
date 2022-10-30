425e11bc4edb6866ac5faaa1fdf98ed20199e2a6
DAZE\User <igli.ylli1594@gmail.com>
2022-10-30 14:12:59 +0100

    finished

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, race } from 'rxjs';
+import { SdaHttpClient } from '../data-layer/sda-be-mock.service';
+import { RatingInterface } from '../Interfaces/rating-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RatingService {
 
-  constructor() { }
+  constructor(private http:SdaHttpClient) { }
+
+   addRating(rating:RatingInterface):Observable<RatingInterface>{
+    return this.http.post('Rating', rating);
+   }
+   
+
+   
+
 }
